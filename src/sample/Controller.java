0beec44840c4ede6df17f228265568f5f2e67350
Ken W. Alger <kenalger@comcast.net>
2015-10-01 23:06:51 -0700

    working on API submission

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,20 +5,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.InvocationTargetException;
 
 public class Controller {
 
@@ -36,36 +30,58 @@ public class Controller {
 
     private String resumeFile;
 
-    public void handleJobSubmittal(ActionEvent actionEvent) throws IOException {
+    public void handleJobSubmittal(ActionEvent actionEvent) throws Exception {
         String projectList = ("{\"" + gitHub.getText() +
                 "\", \"" + personalSite.getText() +
                 "\", \"" + projects.getText() + "\"}");
 
-
-        CloseableHttpClient httpClient = HttpClients.createDefault();
         String postUrl = "https://api.perka.com/1/communication/job/apply";
 
-        HttpPost httpPost = new HttpPost(postUrl);
+        try {
+//            HttpClient httpClient = HttpClients.createDefault();
+//            String postUrl = "https://api.perka.com/1/communication/job/apply";
+//
+//            HttpPost httpPost = new HttpPost(postUrl);
+//
+//            List<NameValuePair> nvps = new ArrayList<>();
+//            nvps.add(new BasicNameValuePair("first_name", firstName.getText()));
+//            nvps.add(new BasicNameValuePair("last_name", lastName.getText()));
+//            nvps.add(new BasicNameValuePair("email", email.getText()));
+//            nvps.add(new BasicNameValuePair("position_id", positionId.getText()));
+//            nvps.add(new BasicNameValuePair("explanation", explanation.getText()));
+//            nvps.add(new BasicNameValuePair("projects", projectList));
+//            nvps.add(new BasicNameValuePair("source", source.getText()));
+//            nvps.add(new BasicNameValuePair("resume", ResumeHandler.encodeFileToBase64Binary(resumeFile)));
+//
+//            httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+//
+//            HttpResponse response = httpClient.execute(httpPost);
+//
+//
+//                System.out.println(response.getStatusLine());
+//                HttpEntity entity = response.getEntity();
+//                EntityUtils.consume(entity);
 
-        List<NameValuePair> nvps = new ArrayList<>();
-        nvps.add(new BasicNameValuePair("first_name", firstName.getText()));
-        nvps.add(new BasicNameValuePair("last_name", lastName.getText()));
-        nvps.add(new BasicNameValuePair("email", email.getText()));
-        nvps.add(new BasicNameValuePair("position_id", positionId.getText()));
-        nvps.add(new BasicNameValuePair("explanation", explanation.getText()));
-        nvps.add(new BasicNameValuePair("projects", projectList));
-        nvps.add(new BasicNameValuePair("source", source.getText()));
-        nvps.add(new BasicNameValuePair("resume", ResumeHandler.encodeFileToBase64Binary(resumeFile)));
+            String appJSON = ("{\"first_name\":\"" + firstName.getText() +
+                    "\",\"last_name\":\"" + lastName.getText() +
+                    "\",\"email\":\"" + email.getText() +
+                    "\",\"positionID\":\"" + positionId.getText() +
+                    "\",\"explanation\":\"" + explanation.getText() +
+                    "\",\"projects\":\"" + projectList +
+                    "\",\"source\":\"" + source.getText() +
+                    "\",\"resume\":\"" + ResumeHandler.encodeFileToBase64Binary(resumeFile) +
+                    "\"}");
 
-        httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+            Request.Post(postUrl)
+                    .bodyForm(Form.form().add(appJSON, "")
+                            .build())
+                    .execute().returnContent();
 
-        try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
-            System.out.println(response.getStatusLine());
-            HttpEntity entity = response.getEntity();
-            EntityUtils.consume(entity);
+
+            applicationSubmitted.setText("You just submitted your application information to Perka.com");
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
-
-        applicationSubmitted.setText("You just submitted your application information to Perka.com");
     }
 
     public void uploadResume(ActionEvent actionEvent) {
@@ -78,17 +94,8 @@ public class Controller {
 
         if (selectedFile != null) {
             fileSelected.setText(selectedFile.toPath().toString());
-            System.out.println("Path: " + selectedFile.toPath());
-            System.out.println("File: " + selectedFile.getName());
 
             resumeFile = selectedFile.toPath().toString();
-
-            try {
-                String resume = ResumeHandler.encodeFileToBase64Binary(resumeFile);
-                System.out.println( resume );
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
         } else {
             fileSelected.setText("PDF Resume file selection cancelled.");
