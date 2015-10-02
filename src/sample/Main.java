d78ecbf56060a0b1d35657ec4cff141ee63e98fe
Ken W. Alger <kenalger@comcast.net>
2015-10-02 07:46:43 -0700

    Code clean-up

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        System.out.println("javafx.runtime.version: " + System.getProperties().get("javafx.runtime.version"));
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Perka Job Application Submission - by Ken W. Alger");
         primaryStage.setScene(new Scene(root, 750, 675)); // width, height
